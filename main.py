79729dc5337b7a04ec0d3a40f9dac4cc0c3ec301
GKroch <g.krochmal@student.uw.edu.pl>
2019-01-18 17:58:15 +0100

    Hope its final

--- main.py
+++ main.py
@@ -94,6 +94,17 @@ def type(date):
 def error_handler():
     return redirect(url_for("/"))
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 
 if __name__ == '__main__':
